be106feff32667c001fb51df79af9af8ff86bbd7
DefineXX <keaikim77@gmail.com>
2025-06-06 20:09:28 +0900

    chore: Guide comment 수정

--- apps/client/app/page.tsx
+++ apps/client/app/page.tsx
@@ -35,8 +35,8 @@ export default function Page() {
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
             <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
                 <p className="fixed left-0 top-0 flex w-full justify-center border border-neutral-800 from-inherit px-4 pb-6 pt-8 backdrop-blur-2xl lg:static lg:w-auto lg:rounded-xl lg:p-4">
-                    examples/with-tailwind -&nbsp;
-                    <code className="font-mono font-bold">web</code>
+                    Loopz/monorepo -&nbsp;
+                    <code className="font-mono font-bold">client</code>
                 </p>
                 <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center lg:static lg:h-auto lg:w-auto">
                     <a
