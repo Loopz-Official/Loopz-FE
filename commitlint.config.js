ac66e68ee829b5b3881570c6c019fe0e5bab6696
DefineXX <keaikim77@gmail.com>
2025-06-06 16:14:14 +0900

    chore: commitlint 설치 및 configuration

---
+++ commitlint.config.js
@@ -0,0 +1,21 @@
+const Configuration = {
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+        'type-enum': [
+            2,
+            'always',
+            [
+                'feat',
+                'fix',
+                'docs',
+                'style',
+                'refactor',
+                'test',
+                'revert',
+                'chore',
+            ],
+        ],
+    },
+};
+
+module.exports = Configuration;
