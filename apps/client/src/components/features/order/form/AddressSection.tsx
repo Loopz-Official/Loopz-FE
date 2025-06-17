32b13f1fe129ce8b47387a6940dd596720755148
young <azerosong@gmail.com>
2025-06-18 00:24:41 +0900

    feat: 배송지 정보가 없을 때 배송지 정보 섹션 ui 추가

--- apps/client/src/components/features/order/form/AddressSection.tsx
+++ apps/client/src/components/features/order/form/AddressSection.tsx
@@ -2,6 +2,8 @@
 
 import { useState } from 'react';
 
+import { PlusIcon } from '@/components/icons/Plus';
+
 const DELIVERY_REQUESTS = [
     '문 앞에 놓아주세요.',
     '부재 시 경비실에 맡겨주세요.',
@@ -15,68 +17,90 @@ export default function AddressSection() {
     const [isOptionsOpen, setIsOptionsOpen] = useState(false);
     const [requestContent, setRequestContent] = useState<string | null>(null);
     const isTextareaOpen = requestContent === DELIVERY_REQUESTS.at(-1);
+    const hasAddressInfo = false;
 
     return (
         <>
             <header className="flex items-center justify-between">
                 <h2 className="text-body-01 font-semibold">배송지 정보</h2>
-                <button className="text-caption-01 rounded-xs border-gray-regular flex w-[3.375rem] items-center justify-center border py-1">
-                    변경
-                </button>
+                {hasAddressInfo && (
+                    <button className="text-caption-01 rounded-xs border-gray-regular flex w-[3.375rem] items-center justify-center border py-1">
+                        변경
+                    </button>
+                )}
             </header>
 
-            <div className="space-y-1.5">
-                <div className="flex items-center gap-1">
-                    <span className="text-body-03 font-semibold">이예나</span>
-                    <span className="text-caption-01 text-point font-semibold">
-                        기본 배송지
-                    </span>
-                </div>
-                <div className="text-caption-01 tracking-normal">
-                    [09876] 서울특별시 영등포구 도신로 29길 28, 103동 801호
-                </div>
-                <div className="text-caption-01 tracking-normal">
-                    010-XXXX-XXXX
-                </div>
-            </div>
+            {hasAddressInfo ? (
+                <>
+                    <div className="space-y-1.5">
+                        <div className="flex items-center gap-1">
+                            <span className="text-body-03 font-semibold">
+                                이예나
+                            </span>
+                            <span className="text-caption-01 text-point font-semibold">
+                                기본 배송지
+                            </span>
+                        </div>
+                        <div className="text-caption-01 tracking-normal">
+                            [09876] 서울특별시 영등포구 도신로 29길 28, 103동
+                            801호
+                        </div>
+                        <div className="text-caption-01 tracking-normal">
+                            010-XXXX-XXXX
+                        </div>
+                    </div>
 
-            {/* 배송 요청사항 */}
-            <div>
-                <button
-                    onClick={() => setIsOptionsOpen(!isOptionsOpen)}
-                    className={`${isOptionsOpen || isTextareaOpen ? 'rounded-t-xs' : 'rounded-xs'} border-gray-regular text-caption-01 w-full border transition-[max-height]`}
-                >
-                    <div
-                        className={`${requestContent ? 'text-black' : 'text-disabled'} flex items-center justify-between px-3 py-2.5`}
-                    >
-                        {requestContent || '배송 요청사항 선택'}
-                        <div className="h-4 w-4 bg-black" />
-                    </div>
-                </button>
-                {isOptionsOpen && (
-                    <div className="border-gray-regular rounded-b-xs text-caption-01 flex flex-col border border-t-0">
-                        {DELIVERY_REQUESTS.map((request) => (
-                            <button
-                                key={request}
-                                onClick={() => {
-                                    setRequestContent(request);
-                                    setIsOptionsOpen(false);
-                                }}
-                                className="active:bg-gray-regular w-full px-4 py-2.5 text-left transition-colors"
+                    {/* 배송 요청사항  */}
+                    <div>
+                        <button
+                            onClick={() => setIsOptionsOpen(!isOptionsOpen)}
+                            className={`${isOptionsOpen || isTextareaOpen ? 'rounded-t-xs' : 'rounded-xs'} border-gray-regular text-caption-01 w-full border transition-[max-height]`}
+                        >
+                            <div
+                                className={`${requestContent ? 'text-black' : 'text-disabled'} flex items-center justify-between px-3 py-2.5`}
                             >
-                                {request}
-                            </button>
-                        ))}
+                                {requestContent || '배송 요청사항 선택'}
+                                <div className="h-4 w-4 bg-black" />
+                            </div>
+                        </button>
+                        {isOptionsOpen && (
+                            <div className="border-gray-regular rounded-b-xs text-caption-01 flex flex-col border border-t-0">
+                                {DELIVERY_REQUESTS.map((request) => (
+                                    <button
+                                        key={request}
+                                        onClick={() => {
+                                            setRequestContent(request);
+                                            setIsOptionsOpen(false);
+                                        }}
+                                        className="active:bg-gray-regular w-full px-4 py-2.5 text-left transition-colors"
+                                    >
+                                        {request}
+                                    </button>
+                                ))}
+                            </div>
+                        )}
+                        {isTextareaOpen && (
+                            <textarea
+                                maxLength={50}
+                                placeholder="내용을 입력해 주세요. (최대 50자)"
+                                className="h-26 placeholder:text-disabled text-caption-01 border-gray-regular rounded-b-xs flex w-full resize-none flex-col border border-t-0 p-4 outline-none"
+                            />
+                        )}
                     </div>
-                )}
-                {isTextareaOpen && (
-                    <textarea
-                        maxLength={50}
-                        placeholder="내용을 입력해 주세요. (최대 50자)"
-                        className="h-26 placeholder:text-disabled text-caption-01 border-gray-regular rounded-b-xs flex w-full resize-none flex-col border border-t-0 p-4 outline-none"
-                    />
-                )}
-            </div>
+                </>
+            ) : (
+                <div className="text-body-03 text-center font-normal">
+                    <div className="text-disabled mb-8 mt-5">
+                        등록된 배송지가 없습니다.
+                        <br />
+                        배송지를 신규입력 해주세요.
+                    </div>
+                    <button className="border-gray-regular flex w-full items-center justify-center gap-1 rounded-[0.25rem] border py-3">
+                        <PlusIcon className="h-4 w-4" />
+                        배송지 추가
+                    </button>
+                </div>
+            )}
         </>
     );
 }
