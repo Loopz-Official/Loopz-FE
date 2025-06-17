e62d56c3f5edb209d1bb98cf142065093020a8c5
young <azerosong@gmail.com>
2025-06-17 14:25:17 +0900

    feat: comming soon 그룹 추가

---
+++ apps/client/src/app/(comming-soon)/layout.tsx
@@ -0,0 +1,15 @@
+import Header from '@/components/common/Header';
+import NavigationBar from '@/components/common/NavigationBar';
+
+export default function Layout() {
+    return (
+        <div>
+            <Header type="title" />
+            <div className="flex h-[calc(100vh-112px)] flex-col items-center justify-center">
+                <span>Loopz의 새로운 기능을 준비하고 있어요.</span>
+                <span>곧 더 좋은 서비스로 찾아뵙겠습니다.</span>
+            </div>
+            <NavigationBar />
+        </div>
+    );
+}
