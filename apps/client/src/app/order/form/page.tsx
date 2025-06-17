e0ca3aa3337d0549c8206731b212c33475e67d3e
young <azerosong@gmail.com>
2025-06-17 22:04:31 +0900

    feat: 결제 금액 섹션 기본 ui 구현

--- apps/client/src/app/order/form/page.tsx
+++ apps/client/src/app/order/form/page.tsx
@@ -1,24 +1,27 @@
 import AddressSection from '@/components/features/order/form/AddressSection';
 import OrderItemsSection from '@/components/features/order/form/OrderItemsSection';
+import PriceSummarySection from '@/components/features/order/form/PriceSummarySection';
 
 export default function Page() {
     return (
         <div>
             {/* 헤더 */}
 
-            <div className="flex flex-col gap-8 px-5 pb-12 pt-2">
+            <div className="flex flex-col px-5 pb-12 pt-2">
                 {/* 배송지 정보 */}
-                <div className="flex flex-col gap-3 border-t border-black pt-4">
+                <div className="flex flex-col gap-3 border-t border-black pb-8 pt-4">
                     <AddressSection />
                 </div>
 
                 {/* 주문 상품 */}
-                <div className="flex flex-col gap-3 border-t border-black pt-4">
+                <div className="flex flex-col gap-3 border-t border-black pb-8 pt-4">
                     <OrderItemsSection />
                 </div>
 
                 {/* 결제 금액 */}
-                <div className="flex flex-col gap-3 border-t border-black pt-5"></div>
+                <div className="flex flex-col border-t border-black pb-5 pt-5">
+                    <PriceSummarySection />
+                </div>
 
                 {/* 약관 동의 */}
                 <div className="flex flex-col gap-3 border-t border-black pt-5"></div>
