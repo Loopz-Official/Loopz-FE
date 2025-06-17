179e0eebc6b9426eb8a57857682ded79893d5a55
young <azerosong@gmail.com>
2025-06-17 13:38:53 +0900

    feat: 헤더 svgr 적용

--- apps/client/src/components/common/Header.tsx
+++ apps/client/src/components/common/Header.tsx
@@ -1,4 +1,6 @@
-type HeaderType = 'main' | 'sub' | 'title' | 'pop-up';
+import { LEFT_SIDE_OPTIONS, RIGHT_SIDE_OPTIONS } from '@/constants/header';
+
+export type HeaderType = 'main' | 'sub' | 'title' | 'pop-up';
 
 export default function Header({
     type,
@@ -7,23 +9,31 @@ export default function Header({
     type: HeaderType;
     title?: string;
 }) {
+    const currentOption = LEFT_SIDE_OPTIONS[type];
+
     return (
         <div className="sticky top-0 z-10 grid h-14 w-full grid-cols-[1fr_auto_1fr] bg-white px-5 py-[0.875rem]">
             <div>
-                {type === 'main' ? (
-                    <div className="ml-1">로고</div>
-                ) : type === 'pop-up' ? (
-                    <button>닫기</button>
-                ) : (
-                    <button>뒤로가기</button>
-                )}
+                <button
+                    className="flex items-center gap-1"
+                    aria-label={currentOption.label}
+                >
+                    <currentOption.icon className={currentOption.className} />
+                </button>
             </div>
             <div className="text-headline-04">{title}</div>
             <div className="flex gap-4 place-self-end">
                 {(type === 'main' || type === 'sub') && (
                     <>
-                        <button>검색</button>
-                        <button>장바구니</button>
+                        {RIGHT_SIDE_OPTIONS.map(({ label, icon: Icon }) => (
+                            <button
+                                key={label}
+                                className="flex items-center gap-1"
+                                aria-label={label}
+                            >
+                                <Icon className="h-7 w-7" />
+                            </button>
+                        ))}
                     </>
                 )}
             </div>
--- apps/client/src/constants/header.ts
+++ apps/client/src/constants/header.ts
@@ -1,19 +1,25 @@
-import * as I from '../components/icons/Header';
+import * as I from '@/components/icons/Header';
 
-export const LEFT_SIDE_OPTIONS = [
-    {
+const BACK_BUTTON = {
+    label: '뒤로가기',
+    icon: I.BackIcon,
+    className: 'w-7 h-7',
+};
+
+export const LEFT_SIDE_OPTIONS = {
+    main: {
         label: 'Loopz',
         icon: I.LoopzIcon,
+        className: 'w-[4.0625rem] h-7',
     },
-    {
-        label: '뒤로가기',
-        icon: I.BackIcon,
-    },
-    {
+    sub: BACK_BUTTON,
+    title: BACK_BUTTON,
+    'pop-up': {
         label: '닫기',
         icon: I.CloseIcon,
+        className: 'w-7 h-7',
     },
-];
+};
 
 export const RIGHT_SIDE_OPTIONS = [
     {
