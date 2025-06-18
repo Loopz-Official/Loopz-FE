83fef1362984f9785dd1575ba3464afe9bd55a6e
young <azerosong@gmail.com>
2025-06-18 15:36:43 +0900

    feat: 배송지 추가 페이지 이름, 체크박스, 버튼 로직 구현

--- apps/client/src/app/addresses/[type]/page.tsx
+++ apps/client/src/app/addresses/[type]/page.tsx
@@ -6,10 +6,19 @@ import { useRef, useState } from 'react';
 import BottomButton from '@/components/features/order/BottomButton';
 
 export default function Page() {
+    const [name, setName] = useState('');
     const [phoneNumber, setPhoneNumber] = useState<string[]>(['', '', '']);
     const [address, setAddress] = useState('');
     const [zonecode, setZonecode] = useState('');
+    const [extraAddress, setExtraAddress] = useState('');
     const maxLengths = [3, 4, 4];
+
+    const isDisabled = !(
+        name &&
+        phoneNumber.every((number) => number) &&
+        address &&
+        zonecode
+    );
 
     const params = useParams();
     const type = params.type;
@@ -56,14 +65,26 @@ export default function Page() {
         <div>
             {/* 헤더 */}
 
-            <div className="h-[calc(100vh-112px)] px-5 py-3">
+            <div className="h-[calc(100vh-112px)] space-y-6 px-5 py-3">
                 {/* 이름 */}
-                <div></div>
+                <div className="text-body-03 font-regular flex flex-col gap-2">
+                    <div>
+                        받으시는 분&nbsp;
+                        <span className="text-point text-body-02">*</span>
+                    </div>
+
+                    <input
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                        placeholder="이름을 입력해 주세요."
+                        className="border-gray-regular rounded-[0.25rem] border px-3 py-2.5 tracking-normal"
+                    />
+                </div>
 
                 {/* 연락처 */}
                 <div className="text-body-03 font-regular flex flex-col gap-2">
-                    <div className="space-x-0.5">
-                        연락처
+                    <div>
+                        연락처&nbsp;
                         <span className="text-point text-body-02">*</span>
                     </div>
 
@@ -89,8 +110,9 @@ export default function Page() {
 
                 {/* 주소 */}
                 <div className="text-body-03 font-regular flex flex-col gap-2">
-                    <div className="space-x-0.5">
-                        주소 <span className="text-point text-body-02">*</span>
+                    <div>
+                        주소&nbsp;
+                        <span className="text-point text-body-02">*</span>
                     </div>
 
                     <div className="flex gap-1.5">
@@ -114,13 +136,29 @@ export default function Page() {
                     />
 
                     <input
+                        value={extraAddress}
+                        onChange={(e) => setExtraAddress(e.target.value)}
                         placeholder="상세 주소를 입력해 주세요."
                         className="border-gray-regular rounded-[0.25rem] border px-3 py-2.5 tracking-normal"
                     />
                 </div>
+
+                <label className="flex items-center gap-2">
+                    <input
+                        type="checkbox"
+                        className="border-gray-09 rounded-xs not-checked:bg-[url('/unchecked-check.svg')] relative h-4 w-4 appearance-none border bg-center bg-no-repeat checked:border-black checked:bg-black checked:bg-[url('/checked-check.svg')]"
+                    />
+                    <span className="text-body-03 cursor-pointer">
+                        기본 배송지로 설정
+                    </span>
+                </label>
             </div>
 
-            <BottomButton text="저장하기" onClick={() => {}} isDisabled />
+            <BottomButton
+                text="저장하기"
+                isDisabled={isDisabled}
+                onClick={() => {}}
+            />
         </div>
     );
 }
