e00d4810cf8b90db323365b2845a9769ead0af2a
young <azerosong@gmail.com>
2025-06-19 11:04:24 +0900

    refactor: 배송 요청사항을 상수로 분리

--- apps/client/src/components/features/order/form/AddressSection.tsx
+++ apps/client/src/components/features/order/form/AddressSection.tsx
@@ -4,15 +4,7 @@ import { useRouter } from 'next/navigation';
 import { Dispatch, SetStateAction, useEffect, useState } from 'react';
 
 import { PlusIcon } from '@/components/icons/Plus';
-
-const DELIVERY_REQUESTS = [
-    '문 앞에 놓아주세요.',
-    '부재 시 경비실에 맡겨주세요.',
-    '택배함에 넣어주세요.',
-    '파손 위험 상품입니다. 배송 시 주의해 주세요.',
-    '배송 전에 꼭 연락 주세요.',
-    '직접 입력',
-];
+import { DELIVERY_REQUESTS } from '@/constants/deliveryRequests';
 
 export default function AddressSection({
     setHasAddressInfo,
