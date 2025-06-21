ec2d30c14169c7104f550333e4837acb6a720678
young <azerosong@gmail.com>
2025-06-22 00:15:40 +0900

    refactor: filter list 상수로 분리

--- apps/client/src/app/filter/page.tsx
+++ apps/client/src/app/filter/page.tsx
@@ -3,47 +3,7 @@
 import Header from '@/components/common/Header';
 import ChipList from '@/components/features/filter/ChipList';
 import PriceRange from '@/components/features/filter/PriceRange';
-
-const filterList = [
-    {
-        title: '상품 정보',
-        chips: ['품절상품 제외'],
-    },
-    {
-        title: '제품 유형',
-        chips: [
-            '가구',
-            '조명',
-            '디스플레이 집기',
-            '오브제 / 소품',
-            '화병 / 화분',
-            '라이프스타일 테크',
-            '예술 / 아트',
-        ],
-    },
-    {
-        title: '제품 크기',
-        chips: ['소형', '중형', '대형'],
-    },
-    {
-        title: '가격대',
-    },
-    {
-        title: '키워드',
-        chips: [
-            '감성적인',
-            '실용적인',
-            '트렌디한',
-            '레트로한',
-            '유니크한',
-            '미니멀한',
-            '심플한',
-            '심오한',
-            '거대한',
-            '아기자기한',
-        ],
-    },
-];
+import { filterList } from '@/constants/filterList';
 
 export default function Page() {
     return (
