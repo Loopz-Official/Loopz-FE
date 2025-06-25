a47b1f15e1033de5504a1a736aea783bb61f8075
DefineXX <keaikim77@gmail.com>
2025-06-25 11:45:05 +0900

    feat: 가입 완료 페이지 구현

--- apps/client/src/app/auth/layout.tsx
+++ apps/client/src/app/auth/layout.tsx
@@ -12,18 +12,27 @@ export default function AuthLayout({
 }) {
     const pathname = usePathname();
     const isLoginPage = pathname === '/auth/login';
+    const isCompletePage = pathname === '/auth/complete';
 
     return (
         <div className="flex h-screen w-full flex-col px-5">
-            {!isLoginPage && <Header type="title" title="회원가입" />}
-            <main
-                className={clsx(
-                    'flex grow flex-col justify-between',
-                    isLoginPage ? 'pt-70 pb-16' : 'pb-16 pt-8'
-                )}
-            >
-                {children}
-            </main>
+            {!isCompletePage ? (
+                <>
+                    {!isLoginPage && <Header type="title" title="회원가입" />}
+                    <main
+                        className={clsx(
+                            'flex grow flex-col justify-between',
+                            isLoginPage ? 'pt-70 pb-16' : 'pb-16 pt-8'
+                        )}
+                    >
+                        {children}
+                    </main>
+                </>
+            ) : (
+                <main className="flex h-full flex-col items-center justify-center">
+                    {children}
+                </main>
+            )}
         </div>
     );
 }
