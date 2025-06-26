5873cc68d84017dae15317b93f6c6f8c601a0256
DefineXX <keaikim77@gmail.com>
2025-06-26 23:44:24 +0900

    feat: axios response interceptor 구현

--- apps/client/src/services/config/axios.ts
+++ apps/client/src/services/config/axios.ts
@@ -24,6 +24,29 @@ apiClient.interceptors.request.use(
     }
 );
 
+// Response Interceptor
+apiClient.interceptors.response.use(
+    (response) => {
+        // Any status code that lie within the range of 2xx cause this function to trigger
+        // Do something with response data
+        return response;
+    },
+    (error) => {
+        // Any status codes that falls outside the range of 2xx cause this function to trigger
+        // Do something with response error
+
+        console.log('Request Error: ', error);
+
+        if (error?.response.status === 401 || error?.response.status === 403) {
+            localStorage.clear();
+            sessionStorage.clear();
+            window.location.href = '/auth/login'; // 추후 가능하면 UX 보완
+        }
+
+        return Promise.reject(error);
+    }
+);
+
 export const apiClientGoogleAuth = axios.create({
     baseURL: OAUTH_GOOGLE_API,
 });
