ab7135c1f982ebe2ed5fbf2ec863256bb8748801
DefineXX <keaikim77@gmail.com>
2025-06-27 15:20:32 +0900

    feat(mypage): 로그아웃 구현 위한 마이페이지 내 로그아웃 버튼 sketch UI 추가

---
+++ apps/client/src/components/features/mypage/LogoutButton.tsx
@@ -0,0 +1,9 @@
+const LogoutButton = () => {
+    return (
+        <button className="border-status-red text-body-01 text-status-red bg-status-red/10 w-full rounded-md border border-solid py-2.5">
+            로그아웃
+        </button>
+    );
+};
+
+export default LogoutButton;
