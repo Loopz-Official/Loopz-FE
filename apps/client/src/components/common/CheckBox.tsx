8094c6b372751ee7f6c1881113c61dae9ba2c837
DefineXX <keaikim77@gmail.com>
2025-07-02 13:25:57 +0900

    refactor: Checkbox componentize

---
+++ apps/client/src/components/common/CheckBox.tsx
@@ -0,0 +1,18 @@
+const CheckBox = ({
+    isChecked,
+    onChange,
+}: {
+    isChecked: boolean;
+    onChange: () => void;
+}) => {
+    return (
+        <input
+            type="checkbox"
+            checked={isChecked}
+            onChange={onChange}
+            className="border-gray-09 rounded-xs not-checked:bg-[url('/unchecked-check.svg')] relative h-4 w-4 cursor-pointer appearance-none border bg-center bg-no-repeat checked:border-black checked:bg-black checked:bg-[url('/checked-check.svg')]"
+        />
+    );
+};
+
+export default CheckBox;
