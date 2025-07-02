197c6e28758c852ef7b2196c448568a27c07c07d
young <azerosong@gmail.com>
2025-07-02 17:47:44 +0900

    fix: 배너 모조 데이터 수정

--- apps/client/src/constants/mockBanner.ts
+++ apps/client/src/constants/mockBanner.ts
@@ -1,26 +1,26 @@
 export const MOCK_BANNER = [
     {
-        imageUrl: '/banner-01.png',
-        title: '자원의 낭비를 지나\n자원의 선순환을 향해',
-        description: '가치를 더하는 새로운 방법을 제시합니다.',
+        imageUrl: '/banner/01.png',
+        title: '대형 오브제 유통 순환 플랫폼',
+        description: '팝업, 전시 공간에 재사용 가능한 대형 가구/구조물',
         embeddedUrl: 'https://loopz-flow.com',
     },
     {
-        imageUrl: '/banner-02.png',
-        title: '화려한 조명,\n그 아래서 빛나던 오브제',
-        description: '이제 당신의 곁에서 빛납니다.',
-        embeddedUrl: 'https://loopz-flow.com/Itemlist',
+        imageUrl: '/banner/02.png',
+        title: 'POPGA',
+        description: '팝업스토어 정보가 필요할 땐 팝가에서 혜택 받기',
+        embeddedUrl: 'https://popga.co.kr',
     },
     {
-        imageUrl: '/banner-03.png',
-        title: '한 번의 전시로 사라지는\n대형 구축물과 가구들',
-        description: '우리는 지속 가능한 선택을 제안합니다.',
-        embeddedUrl: 'https://loopz-flow.com/brand',
+        imageUrl: '/banner/03.png',
+        title: '우리는 한 번만 쓰이던 이야기에\n다음 장을 엽니다.',
+        description: 'Loopz의 철학이 궁금하다면',
+        embeddedUrl: 'https://loopz.co.kr/about',
     },
-    {
-        imageUrl: '/banner-04.png',
-        title: '지속 가능한 흐름을 위해\nLOOPZ와 함께하세요.',
-        description: '유휴 자원, 폐기가 아닌 선택하세요.',
-        embeddedUrl: 'https://loopz-flow.com/ContactUs',
-    },
+    // {
+    //     imageUrl: '/banner-04.png',
+    //     title: '지속 가능한 흐름을 위해\nLOOPZ와 함께하세요.',
+    //     description: '유휴 자원, 폐기가 아닌 선택하세요.',
+    //     embeddedUrl: 'https://loopz-flow.com/ContactUs',
+    // },
 ];
