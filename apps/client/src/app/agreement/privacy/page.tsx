97684600cb8f9df34d3a885039caeffb1b8b5c16
young <azerosong@gmail.com>
2025-07-09 12:38:44 +0900

    fix: perpose -> purpose 오타 수정

--- apps/client/src/app/agreement/privacy/page.tsx
+++ apps/client/src/app/agreement/privacy/page.tsx
@@ -3,28 +3,28 @@ import Header from '@/components/layouts/Header';
 const PRIVACY_AGREEMENT = [
     {
         title: '회원가입 및 관리',
-        perpose:
+        purpose:
             '회원가입 의사 확인, 회원제 서비스 제공 에 따른 본인 식별·인증, 연령인증, 회원자격 유지·관리, 서비스 부정이용 방지, 각종고지·통지',
         item: '이메일, 이름, 휴대폰 번호, SNS 간편가입 업체의 사용자별 고유키',
         period: '회원 탈퇴 후 3개월까지\n※ 단 관계법령 위반에 따른 수사, 조사등이 진행 중인 경우에는 해당 수사, 조사 종료시까지',
     },
     {
         title: '결제 및 배송 서비스',
-        perpose:
+        purpose:
             '상품(서비스) 배송(전송), 반품, 고객상담 등 구매계약의 이행을 위해 필요한 업무의 처리',
         item: '구매자 정보(이메일, 이름, 휴대폰 번호), 수령인 정보(이름, 휴대폰 번호, 배송 주소), 주문정보\n\n-신용카드 결제 시 : 카드사명, 카드번호\n-계좌이체 시: 예금주명, 계좌번호, 은행명\n-휴대폰 결제 시 : 통신사, 휴대폰번호',
         period: '동의철회 또는 회원 탈퇴시까지\n※ 단, 법정 의무 보유기간에 따라 보관',
     },
     {
         title: '고충처리',
-        perpose:
+        purpose:
             '고객의 신원 확인, 고충사항 확인, 사실조사를 위한 연락·통지, 처리결과 통보',
         item: '이름, 휴대폰 번호, 이메일, 배송주소, 상담 내용, 주문 정보',
         period: '회원 탈퇴시까지\n※ 단, 법정 의무 보유기간에 따라 보관',
     },
     {
         title: '결제 프로세스',
-        perpose: '결제업무 처리',
+        purpose: '결제업무 처리',
         item: '[필수] 성명, 생년월일, 주소, 전화번호, 이메일 주소, 결제수단 정보(신용카드 번호, 유효기간, 카드 소유자명, 은행 계좌 정보, 계좌주명)',
         period: '동의철회 또는 회원 탈퇴시까지\n※ 단, 법정 의무 보유기간에 따라 보관',
     },
@@ -78,7 +78,7 @@ export default function Page() {
                                     {item.title}
                                 </td>
                                 <td className="border-disabled border px-2 pb-5 pt-2.5 align-top">
-                                    {item.perpose}
+                                    {item.purpose}
                                 </td>
                                 <td className="border-disabled border px-2 pb-5 pt-2.5 align-top">
                                     {item.item}
