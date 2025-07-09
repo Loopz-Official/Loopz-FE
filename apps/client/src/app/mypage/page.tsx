b4c8877841b7eb85950c1a0de94436861dff84a3
young <azerosong@gmail.com>
2025-07-09 16:23:22 +0900

    feat: 마이페이지 배너 추가

--- apps/client/src/app/mypage/page.tsx
+++ apps/client/src/app/mypage/page.tsx
@@ -1,5 +1,8 @@
+import Link from 'next/link';
+
 import Button from '@/components/features/mypage/Button';
 import CustomLink from '@/components/features/mypage/CustomLink';
+import * as I from '@/icons/Mypage';
 
 const MYPAGE_LINKS = [
     {
@@ -25,6 +28,23 @@ const MYPAGE_BUTTONS = ['로그아웃', '탈퇴하기'] as const;
 export default function MyPage() {
     return (
         <div>
+            <Link
+                href="/like"
+                className="bg-gray-12 rounded-xs mb-5 grid grid-cols-[auto_1fr] gap-4"
+            >
+                <div className="bg-gray-02 rounded-l-xs relative aspect-square h-full w-auto bg-[url('/mypage-banner.svg')] bg-[11px_15px] bg-no-repeat pl-3 pt-4">
+                    <I.HeartIcon className="absolute left-5 top-7 z-10" />
+                </div>
+                <div className="py-5">
+                    <div className="text-body-03 font-semibold">
+                        찜한 상품만 모아보는 나만의 리스트
+                    </div>
+                    <div className="text-caption-01 text-gray-dark">
+                        관심있는 아이템을 한 곳에 모아보세요
+                    </div>
+                </div>
+            </Link>
+
             <div className="mb-10">
                 {MYPAGE_LINKS.map(({ label, href }) => (
                     <CustomLink key={label} href={href}>
