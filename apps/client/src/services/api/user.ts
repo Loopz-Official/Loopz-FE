1d84b5484dacab9f5435450633f11cd9f1818b43
DefineXX <keaikim77@gmail.com>
2025-07-11 19:28:38 +0900

    feat: 마이페이지 Profile Section API connect

---
+++ apps/client/src/services/api/user.ts
@@ -0,0 +1,24 @@
+import { myAccountInfo } from '@/schemas/user';
+import { validate } from '@/schemas/utils/validate';
+
+import { apiClient } from '../config/axios';
+
+export const getMyAccountInfo = async () => {
+    try {
+        const response = await apiClient.get('/user/v1/me');
+
+        console.log('My Account Info Response:', response);
+
+        if (response.status === 200) {
+            return validate(
+                myAccountInfo,
+                response.data.data,
+                'My Account Info'
+            );
+        }
+        throw new Error('Failed to fetch my account info');
+    } catch (error) {
+        console.error('Error fetching my account info:', error);
+        throw error;
+    }
+};
