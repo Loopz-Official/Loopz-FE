d11e61fd07c74d9087c9d7dc59e526c73331284c
DefineXX <keaikim77@gmail.com>
2025-07-12 21:08:22 +0900

    fix: Nested button tag to Link tag with div tag (w/ button role & tabIndex)

--- apps/client/src/components/features/main/ProductItem.tsx
+++ apps/client/src/components/features/main/ProductItem.tsx
@@ -17,10 +17,30 @@ const ProductItem = memo(function ProductItem({ product }: ProductItemProps) {
         ? { fill: '#FFF', stroke: '#FFF' }
         : { fill: '#00000008', stroke: '#FFF' };
 
-    const handleLikeButtonClick = useCallback((e: React.MouseEvent) => {
-        e.preventDefault();
+    const handleLike = () => {
+        // TODO: 실제 좋아요 API 호출 로직을 여기에 작성
+        // 예: likeProduct(product.objectId)
         alert('like!');
-    }, []);
+    };
+
+    const handleLikeButtonClick = useCallback(
+        (e: React.MouseEvent<HTMLDivElement>) => {
+            e.preventDefault();
+            handleLike();
+        },
+        []
+    );
+
+    const handleLikeButtonKeyDown = useCallback(
+        (e: React.KeyboardEvent<HTMLDivElement>) => {
+            // For button tag default action
+            if (e.key === 'Enter' || e.key === ' ') {
+                e.preventDefault();
+                handleLike();
+            }
+        },
+        []
+    );
 
     return (
         <Link
@@ -41,15 +61,18 @@ const ProductItem = memo(function ProductItem({ product }: ProductItemProps) {
                         SOLD OUT
                     </div>
                 ) : (
-                    <button
+                    <div
                         onClick={handleLikeButtonClick}
-                        className="absolute bottom-2 right-2 z-10"
+                        onKeyDown={handleLikeButtonKeyDown}
+                        className="absolute bottom-2 right-2 z-10 cursor-pointer"
+                        role="button"
+                        tabIndex={0}
                     >
                         <LikeIconDynamic
                             fill={likeIconStyling.fill}
                             stroke={likeIconStyling.stroke}
                         />
-                    </button>
+                    </div>
                 )}
                 <Image
                     src={product.imageUrl}
