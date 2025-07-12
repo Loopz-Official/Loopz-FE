b348ca6b93d25fd3d6609b19e9c1db0969629243
young <azerosong@gmail.com>
2025-07-12 15:05:20 +0900

    chore: default 메타태그 추가

--- apps/client/src/app/layout.tsx
+++ apps/client/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import localFont from 'next/font/local';
 
 import Frame from '@/components/layouts/Frame';
+import { META } from '@/constants/meta';
 import ReactQueryProviderWrapper from '@/providers/QueryProviderWrapper';
 
 import type { Metadata } from 'next';
@@ -15,8 +16,33 @@ const pretendard = localFont({
 });
 
 export const metadata: Metadata = {
-    title: 'Loopz',
-    description: 'Loopz web application',
+    metadataBase: new URL(META.URL),
+
+    title: META.TITLE,
+    description: META.DESCRIPTION,
+    keywords: META.KEYWORDS,
+
+    openGraph: {
+        title: META.TITLE,
+        description: META.DESCRIPTION,
+        images: [
+            {
+                url: META.IMAGE_URL,
+                width: 800,
+                height: 600,
+                alt: META.TITLE,
+            },
+        ],
+        locale: META.LOCALE,
+        type: META.TYPE,
+    },
+    twitter: {
+        title: META.TITLE,
+        description: META.DESCRIPTION,
+        images: {
+            url: META.IMAGE_URL,
+        },
+    },
 };
 
 export default function RootLayout({
---
+++ apps/client/src/constants/meta.ts
@@ -0,0 +1,18 @@
+export const META = {
+    URL: 'https://loopz.co.kr',
+    TITLE: '룹즈',
+    DESCRIPTION:
+        '일회성 설치와 폐기의 관행을 넘어 지속 가능한 전시/팝업 생태계로',
+    IMAGE_URL: '/about/loop.svg',
+    KEYWORDS: [
+        'Loopz',
+        '룹즈',
+        '팝업',
+        '팝업스토어',
+        '전시',
+        '공연',
+        '인테리어',
+    ],
+    LOCALE: 'ko_KR' as const,
+    TYPE: 'website' as const,
+};
