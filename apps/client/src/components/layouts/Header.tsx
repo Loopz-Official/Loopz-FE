6ac669105a58beab4c94400b1f6df0e4b315b309
DefineXX <keaikim77@gmail.com>
2025-07-14 15:29:31 +0900

    fix: Header & GNB z-index to 100

--- apps/client/src/components/layouts/Header.tsx
+++ apps/client/src/components/layouts/Header.tsx
@@ -42,9 +42,7 @@ export default function Header({ type, title, redirectAction }: HeaderProps) {
     };
 
     return (
-        <div
-            className={`sticky top-0 z-20 grid h-14 w-full grid-cols-[1fr_auto_1fr] bg-white px-5 py-[0.875rem]`}
-        >
+        <div className="sticky top-0 z-20 grid h-14 w-full grid-cols-[1fr_auto_1fr] bg-white px-5 py-[0.875rem]">
             <div>
                 <button
                     onClick={handleLeftOptionClick}
