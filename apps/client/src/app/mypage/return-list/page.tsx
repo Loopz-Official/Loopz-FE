e9b893cc4d24ab8bb02961975314f4e626b65646
young <azerosong@gmail.com>
2025-07-15 15:32:31 +0900

    feat: 취소 상세 컴포넌트 구현

--- apps/client/src/app/mypage/return-list/page.tsx
+++ apps/client/src/app/mypage/return-list/page.tsx
@@ -1,9 +1,11 @@
 import ProductItemByDeliveryState from '@/components/features/mypage/order-return-list/ProductItemByDeliveryState';
+import ReturnDetail from '@/components/features/mypage/return-list/ReturnDetail';
 
 export default function Page() {
     return (
         <div>
             <ProductItemByDeliveryState type="return" />
+            <ReturnDetail />
         </div>
     );
 }
