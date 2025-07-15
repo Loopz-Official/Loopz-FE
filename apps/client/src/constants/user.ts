dfc0fe72122b2dd473d402efb91612a2f9156646
DefineXX <keaikim77@gmail.com>
2025-07-15 15:55:28 +0900

    fix: 마이페이지 메인 내 Account indicator

--- apps/client/src/constants/user.ts
+++ apps/client/src/constants/user.ts
@@ -1,10 +1,11 @@
+import { GoogleBadge, KakaoBadge, NaverBadge } from '@/icons/OAuthBadge';
 import { MyAccountInfo } from '@/schemas/user';
 
-export const SOCIAL_LOGIN_TYPE_LABEL: Record<
+export const OAUTH_BADGE: Record<
     MyAccountInfo['socialLoginType'],
-    string
+    React.ComponentType<React.SVGProps<SVGSVGElement>> | undefined
 > = {
-    GOOGLE: '구글',
-    KAKAO: '카카오',
-    NAVER: '네이버',
+    GOOGLE: GoogleBadge,
+    KAKAO: KakaoBadge,
+    NAVER: NaverBadge,
 };
