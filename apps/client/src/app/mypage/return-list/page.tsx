66acb26f0d7c2cc064d372e3a0c91c12e700c46e
young <azerosong@gmail.com>
2025-07-18 01:11:15 +0900

    fix: 취소/반품 내역 복구

--- apps/client/src/app/mypage/return-list/page.tsx
+++ apps/client/src/app/mypage/return-list/page.tsx
@@ -2,7 +2,6 @@
 
 import { useState } from 'react';
 
-import Divider from '@/components/features/mypage/Divider';
 import TabBar from '@/components/features/mypage/list/TabBar';
 import Header from '@/components/layouts/Header';
 import { RETURN_LIST_TABS } from '@/constants/mypage';
@@ -19,22 +18,25 @@ export default function Page() {
                 onClick={(tab) => setSelectedTab(tab)}
             />
 
-            {[[0, 1], [2], [3]].map((arr) => (
+            <div className="text-body-03 fixed left-0 top-0 flex h-dvh w-dvw items-center justify-center font-normal">
+                아직 준비 준비 중인 서비스입니다.
+            </div>
+            {/* {[[0, 1], [2], [3]].map((arr) => (
                 <div key={arr[0]}>
                     <div className="space-y-4 px-5 py-6">
                         <div className="text-headline-04">2025.04.25</div>
                         <div className="space-y-5">
-                            {/* {arr.map((item) => (
+                            {arr.map((item) => (
                                 <ProductItemByDeliveryState
                                     key={item}
                                     type="return"
                                 />
-                            ))} */}
+                            ))}
                         </div>
                     </div>
                     <Divider />
                 </div>
-            ))}
+            ))} */}
         </div>
     );
 }
