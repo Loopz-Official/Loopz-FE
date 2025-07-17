3375d80c99371a2039bad944d4a441a9dd36f333
young <azerosong@gmail.com>
2025-07-18 00:56:02 +0900

    feat: 주문 내역 내부 환불 규정 임시 추가

--- apps/client/src/app/mypage/order-list/page.tsx
+++ apps/client/src/app/mypage/order-list/page.tsx
@@ -6,6 +6,7 @@ import Divider from '@/components/features/mypage/Divider';
 import NoItem from '@/components/features/mypage/list/NoItem';
 // import TabBar from '@/components/features/mypage/list/TabBar';
 import ProductItemByDeliveryState from '@/components/features/mypage/ProductItemByDeliveryState';
+import RefundPolicy from '@/components/features/mypage/return-form/RefundPolicy';
 import Header from '@/components/layouts/Header';
 // import { ORDER_LIST_TABS } from '@/constants/mypage';
 import { useOrderHistoryQuery } from '@/hooks/queries/useOrderQuery';
@@ -47,6 +48,10 @@ export default function Page() {
                     </div>
                 ))
             )}
+
+            <div className="px-5 py-6">
+                <RefundPolicy />
+            </div>
         </div>
     );
 }
