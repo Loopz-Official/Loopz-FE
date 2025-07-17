59e1b51940cf87e445a9fdea9ac4d1835c0f5e19
young <azerosong@gmail.com>
2025-07-17 16:06:10 +0900

    feat: 상품 내역이 없는 경우 UI 추가

--- apps/client/src/app/mypage/order-list/page.tsx
+++ apps/client/src/app/mypage/order-list/page.tsx
@@ -2,9 +2,8 @@
 
 import { useState } from 'react';
 
-import Divider from '@/components/features/mypage/Divider';
+import NoItem from '@/components/features/mypage/list/NoItem';
 import TabBar from '@/components/features/mypage/list/TabBar';
-import ProductItemByDeliveryState from '@/components/features/mypage/ProductItemByDeliveryState';
 import Header from '@/components/layouts/Header';
 import { ORDER_LIST_TABS } from '@/constants/mypage';
 
@@ -12,16 +11,16 @@ export default function Page() {
     const [selectedTab, setSelectedTab] = useState(ORDER_LIST_TABS[0]!.label);
 
     return (
-        <>
-            <div>
-                <Header type="title" title="주문 내역" />
-                <TabBar
-                    tabs={ORDER_LIST_TABS}
-                    selectedTab={selectedTab}
-                    onClick={(tab) => setSelectedTab(tab)}
-                />
+        <div>
+            <Header type="title" title="주문 내역" />
+            <TabBar
+                tabs={ORDER_LIST_TABS}
+                selectedTab={selectedTab}
+                onClick={(tab) => setSelectedTab(tab)}
+            />
 
-                {[[0, 1], [2], [3]].map((arr) => (
+            <NoItem />
+            {/* {[[0, 1], [2], [3]].map((arr) => (
                     <div key={arr[0]}>
                         <div className="space-y-4 px-5 py-6">
                             <div className="text-headline-04">2025.04.25</div>
@@ -36,8 +35,7 @@ export default function Page() {
                         </div>
                         <Divider />
                     </div>
-                ))}
-            </div>
-        </>
+                ))} */}
+        </div>
     );
 }
