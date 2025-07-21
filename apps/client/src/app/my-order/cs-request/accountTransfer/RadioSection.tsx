988e2c0a99c3d270fb975c271356dc4340383c0a
DefineXX <keaikim77@gmail.com>
2025-07-22 01:33:04 +0900

    fix: 주문 내역 관련 기존 컴포넌트 삭제 및 계좌 이체 관련 컴포넌트 directory change

--- apps/client/src/app/my-order/cs-request/accountTransfer/RadioSection.tsx
+++ apps/client/src/app/my-order/cs-request/accountTransfer/RadioSection.tsx
@@ -1,5 +1,3 @@
-import Radio from '@/components/common/Radio';
-
 export default function RadioSection({ options }: { options: string[] }) {
     return (
         <div>
@@ -9,7 +7,7 @@ export default function RadioSection({ options }: { options: string[] }) {
                         key={reason}
                         className="text-body-02 text-gray-dark flex items-center gap-2 font-normal"
                     >
-                        <Radio name="return" />
+                        {/* <Radio name="return" /> */}
                         {reason}
                     </label>
                 ))}
