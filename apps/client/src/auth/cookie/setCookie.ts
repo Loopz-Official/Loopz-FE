963cd66d7c6c6def516a717daaa0c3ddd1e6ea5b
DefineXX <keaikim77@gmail.com>
2025-07-23 04:11:51 +0900

    fix: Browser cookie store logic coping with partial key

--- apps/client/src/auth/cookie/setCookie.ts
+++ apps/client/src/auth/cookie/setCookie.ts
@@ -1,17 +1,16 @@
-type AuthCookies = {
+import { mergeCookieValues } from './mergeCookieValues';
+
+export type AuthCookies = {
     accessToken: string;
     enabled: boolean;
     nickName: string | null;
 };
 
-export const setAuthCookies = ({
-    accessToken,
-    enabled,
-    nickName,
-}: Partial<AuthCookies>) => {
+export const setAuthCookies = (partial: Partial<AuthCookies>) => {
     const expires = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000); // 7일
+    const merged = mergeCookieValues(partial);
 
-    document.cookie = `access-token=${accessToken}; path=/; expires=${expires.toUTCString()};`;
-    document.cookie = `enabled=${enabled}; path=/; expires=${expires.toUTCString()};`;
-    document.cookie = `nickname=${nickName || 'null'}; path=/; expires=${expires.toUTCString()};`;
+    document.cookie = `access-token=${merged.accessToken || ''}; path=/; expires=${expires.toUTCString()};`;
+    document.cookie = `enabled=${merged.enabled ?? ''}; path=/; expires=${expires.toUTCString()};`;
+    document.cookie = `nickname=${merged.nickName || 'null'}; path=/; expires=${expires.toUTCString()};`;
 };
