9ba64dffa4f47ed837c0a95d9376d250643f955d
young <azerosong@gmail.com>
2025-08-02 23:24:05 +0900

    feat: 주문 취소 reason selector 기반으로 탈퇴 페이지 구현

--- apps/client/src/app/mypage/leave/page.tsx
+++ apps/client/src/app/mypage/leave/page.tsx
@@ -1,13 +1,55 @@
+'use client';
+
+import { useState } from 'react';
+
+import RadioButton from '@/components/common/Button/Radio';
 import Header from '@/components/layouts/Header';
+import { LEAVE_REASONS } from '@/constants/user';
 
 export default function Page() {
+    const [selectedValue, setSelectedValue] = useState<string>(
+        LEAVE_REASONS[0] ?? ''
+    );
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setSelectedValue(e.target.value);
+    };
+
     return (
         <>
             <Header type="title" title="탈퇴하기" />
 
-            <div className="px-5 pt-8">
-                <div className="">(필수) 탈퇴 사유를 선택해 주세요.</div>
-            </div>
+            <section className="mb-5 px-5 pt-8">
+                <div className="text-body-01 font-semibold">
+                    (필수) 탈퇴 사유를 선택해 주세요.
+                </div>
+            </section>
+
+            <section className="px-5">
+                <div className="space-y-5">
+                    {LEAVE_REASONS.map((reason) => (
+                        <RadioButton
+                            key={reason}
+                            name={'leave'}
+                            value={reason}
+                            label={reason}
+                            onChange={handleChange}
+                            checked={selectedValue === reason}
+                            className="text-body-02 text-gray-dark flex w-fit items-center gap-2 font-normal"
+                        />
+                    ))}
+                </div>
+
+                {selectedValue === '기타' && (
+                    <div className="pl-8">
+                        <textarea
+                            placeholder="30자 이내로 내용을 입력해주세요."
+                            maxLength={30}
+                            className="border-gray-regular text-body-03 mt-2 h-16 w-full resize-none rounded-sm border p-2.5 font-normal text-black"
+                        />
+                    </div>
+                )}
+            </section>
         </>
     );
 }
